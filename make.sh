0641985751811c799fc4171af4a9c3da8cc3457a
Rainer Rillke <rillke@wikipedia.de>
2015-01-04 11:57:10 +0100

    Update make.sh
    This at least runs on my system; linking statically and then compiling to JS appears to be a better approach, though

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd `dirname $0`
 cd opus-tools
 rm libogg.a
 rm libopus.a
@@ -8,34 +7,68 @@ rm opusenc.so
 cd ..
 
 cd libogg
+echo -----------------------------------
+echo Making libogg
+echo -----------------------------------
 ./autogen.sh
 emconfigure ./configure
 emmake make
-# install
-cp ./src/.libs/libogg.a ../opus-tools
-cd ..
-
-cd opus
-./autogen.sh
-emconfigure ./configure CFLAGS="-O3" --disable-extra-programs
-emmake make
-# install
-cp ./.libs/libopus.a ../opus-tools
+export OGGDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/
 cd ..
 
 cd flac
+echo -----------------------------------
+echo Making flac
+echo -----------------------------------
 ./autogen.sh
-emconfigure ./configure
+emconfigure ./configure --with-ogg=$OGGDIR
+cd ../libogg
+ln -s src/.libs lib
+ln -s include/ogg ogg
+cd ../flac
 emmake make
-# install
-cp ./src/libFLAC/.libs/libFLAC-static.a ../opus-tools
+export FLACDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/
+cd ..
+
+cd opus
+echo -----------------------------------
+echo Making opus
+echo -----------------------------------
+./autogen.sh
+emconfigure ./configure CFLAGS="-O3" --disable-extra-programs
+emmake make
+export OPUSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/
+echo ---------------------------
+echo Opus directory
+echo $OPUSDIR
+echo ---------------------------
 cd ..
 
 cd opus-tools
+echo -----------------------------------
+echo Building Opus-tools
+echo -----------------------------------
 ./autogen.sh
+export OGG_CFLAGS="-I"$OGGDIR"ogg/"
+export OGG_LIBS="-L"$OGGDIR"lib/"
+export OPUS_CFLAGS="-I"$OPUSDIR"include/"
+export OPUS_LIBS="-L"$OPUSDIR".libs/"
+export FLAC_CFLAGS="-I"$FLACDIR"include/FLAC/"
+export FLAC_LIBS="-I"$FLACDIR"src/libFLAC/.libs/"
 emconfigure ./configure
+ln -s $OGGDIR"include/ogg" ogg
+ln -s $FLACDIR"include/FLAC/" FLAC
 emmake make
-# install
 
 mv opusenc opusenc.so
-em++ -O3 opusenc.so -o opusenc.html -s EXPORTED_FUNCTIONS="['_opus_decoder_create', '_opus_decode_float', '_opus_decoder_destroy', '_encode_buffer', '_main']" -s RESERVED_FUNCTION_POINTERS=1
+echo -----------------------------------
+echo Building JavaScript
+echo -----------------------------------
+em++ -O3 $OGGDIR"lib/libogg.so" $FLACDIR"src/libFLAC/.libs/libFLAC.so" $OPUSDIR".libs/libopus.so" opusenc.so -o opusenc.html -s EXPORTED_FUNCTIONS="['_opus_decoder_create', '_opus_decode_float', '_opus_decoder_destroy', '_encode_buffer', '_main']" -s RESERVED_FUNCTION_POINTERS=1
+cp -f opusenc.js ../worker/opusenc.js
+cp -f opusenc.html.mem ../worker/opusenc.data.js
+cd ..
+echo -----------------------------------
+echo Running Press Ctrl+C to abort
+echo -----------------------------------
+emrun iframe.html
